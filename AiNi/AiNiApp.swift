5e5daee7d32e5ded1398afcc5fbc6b4e68d45a2f
Anderson Sprenger <anderson.sprenger@edu.pucrs.br>
2021-05-05 14:35:55 -0300

    add project files

---
+++ AiNi/AiNiApp.swift
@@ -0,0 +1,17 @@
+//
+//  AiNiApp.swift
+//  AiNi
+//
+//  Created by Anderson Sprenger on 05/05/21.
+//
+
+import SwiftUI
+
+@main
+struct AiNiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
