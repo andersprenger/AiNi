54287b846186dcde04c95202624df9a806c79e5f
Heitor <heitorkunrath@gmai.com>
2021-05-18 15:16:09 -0300

    Create New Treatment com formato geral pronto, sem back end e sem uma boa formatação

---
+++ AiNi/Professional/ListedPacientView.swift
@@ -0,0 +1,35 @@
+//
+//  ListedPacientView.swift
+//  AiNi
+//
+//  Created by Heitor Feijó Kunrath on 18/05/21.
+//
+
+import SwiftUI
+
+struct ListedPacientView: View {
+    var body: some View {
+        ZStack{
+            HStack{
+                
+                Image("patient-image")
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 5))
+                
+                Text("Robertinho")
+                
+                
+                
+            }
+        }
+        
+    }
+}
+
+struct ListedPacientView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListedPacientView()
+    }
+}
--- AiNi/Professional/Treatments/CreateNewTreatment.swift
+++ AiNi/Professional/Treatments/CreateNewTreatment.swift
@@ -11,34 +11,66 @@ struct CreateNewTreatment: View {
     
     @State var name : String = ""
     @State var description : String = ""
+    @State var pacientName : String = ""
     var body: some View {
         NavigationView{
             
-        VStack{
-            ZStack{
-            ZStack {
-                TextField("Nome do Tratamento", text: $name)
+            VStack{
+                ZStack{
+                    ZStack {
+                        TextField("Nome do Tratamento", text: $name)
+                        
+                    }.padding()
+                    .background(CardsGradientStyle().opacity(0.1))
+                    .cornerRadius(15)
+                    
+                    
+                    
+                }
+                ZStack {
+                    
+                    TextField("Descrição", text: $description)
+                    
+                }.padding()
+                .background(CardsGradientStyle().opacity(0.1))
+                .cornerRadius(15)
+                ZStack{
+                    VStack{
+                        HStack{
+                            Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                                Image(systemName: "plus")
+                                Text("Adicionar Pacientes")
+                            }
+                            Spacer()
+                        }
+                        HStack{
+                            TextField("Pesquisar Pacientes", text: $pacientName).background(Color.white)
+                            
+                            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                                Text("Adicionar").font(.custom("SF Pro Display", size: 12)).padding()
+                            }).background(Color.white)
+                            
+                            
+                        }
+                        
+                        ForEach(1...5, id: \.self) {_ in
+                            HStack{
+                                ListedPacientView()
+                                Spacer()
+                                Image(systemName: "minus.circle")
+                            }
+                        }
+                        
+                    }.padding()
+                }.background(CardsGradientStyle().opacity(0.1))
                 
-            }.padding()
-            .background(LinearGradient(gradient: Gradient(colors: [.white, .red, .black]), startPoint: .leading, endPoint: .trailing).opacity(0.1))
-            .cornerRadius(15)
-            
                 
-                
-            }
-            ZStack {
-                
-                TextField("Descrição", text: $description)
-                
-            }.padding()
-            .background(LinearGradient(gradient: Gradient(colors: [.white, .red, .black]), startPoint: .leading, endPoint: .trailing).opacity(0.1))
-            .cornerRadius(15)
+                BlueButton()
+
+            }.navigationBarHidden(true)
+            .padding()
             
-            
-            
-            
-        }.navigationBarHidden(true)
-            
+                        
         }.navigationBarTitle("bergmann")
     }
 }
