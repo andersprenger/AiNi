c149cf61ef03599a1ef77c01de38a8fb89507fdb
Anderson Sprenger <anderson.sprenger@edu.pucrs.br>
2021-05-20 18:28:25 -0300

    navigationitens in treatmentprostages

--- AiNi/Professional/Treatments/TreatmentProStages.swift
+++ AiNi/Professional/Treatments/TreatmentProStages.swift
@@ -19,73 +19,80 @@ struct TreatmentProStages: View {
     @State private var selectedStage: StepDetailsModel?
     
     var body: some View {
-        NavigationView{
-            VStack{
-                ScrollView {
-                    VStack (alignment: .center, spacing: 15) {
-                        Text("Crie o seu tratamento do jeito que quiser, adicione etapas, tarefas, frequência e tempo.")
-                            .font(.footnote)
-                            .frame(width: 339, alignment: .center)
-                            .padding(.top, 15)
-                        
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 10.0).fill(LinearGradient(gradient: Gradient(colors: [Color(.systemBlue), Color(.systemIndigo)]), startPoint: .leading, endPoint: .trailing)).opacity(0.1)
-                                .frame(height: 46, alignment: .center)
-                                .foregroundColor(Color("card-color"))
-                            
-                            Text("\(Image(systemName: "plus")) Nova Etapa")
-                                .font(.footnote).foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                                .sheet(isPresented: $novaEtapa) {
-                                    ModalNewStep(viewModel : StepDetailsModel(title: "Nova Etapa", stepByStep: "", activityTime: false, frequency: false),completeStep: completeStep)
-                                }
-                                .onTapGesture {
-                                    novaEtapa = true
-                                }
-                        }
-                        ForEach(viewModel.stepList) { stepItem in
-                            Button(action: {
-                                selectedStage = stepItem
-
-                            }){
-                                Text(stepItem.title)
-                                    .foregroundColor(.white)
-
-                            }.buttonStyle(StageCard())
-                            .background(CardsGradientStyle())
-                            .cornerRadius(10)
-                            
-                            
-                        }.sheet(item: $selectedStage) { step in
-                            ModalNewStep(viewModel: step, completeStep: completeStep)
-                            
-                        }
-                    }
-                    .padding(.horizontal)
-                    
-                    
-                    
-                }
-                Spacer()
-                ZStack {
-                    RoundedRectangle(cornerRadius: 10.0) .fill(LinearGradient(gradient: Gradient(colors: [Color(.systemBlue), Color(.systemIndigo)]), startPoint: .leading, endPoint: .trailing))
-                        .frame(width: 150, height: 45, alignment: .center)
-                    
-                    
-                    Text("Seguinte").foregroundColor(.white)
+        VStack{
+            ScrollView {
+                VStack (alignment: .center, spacing: 15) {
+                    Text("Crie o seu tratamento do jeito que quiser, adicione etapas, tarefas, frequência e tempo.")
                         .font(.footnote)
+                        .frame(width: 339, alignment: .center)
+                        .padding(.top, 15)
                     
-                    
-                }.padding()
-                .onTapGesture {
-                    /////////////
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 10.0).fill(LinearGradient(gradient: Gradient(colors: [Color(.systemBlue), Color(.systemIndigo)]), startPoint: .leading, endPoint: .trailing)).opacity(0.1)
+                            .frame(height: 46, alignment: .center)
+                            .foregroundColor(Color("card-color"))
+                        
+                        Text("\(Image(systemName: "plus")) Nova Etapa")
+                            .font(.footnote).foregroundColor(.blue)
+                            .sheet(isPresented: $novaEtapa) {
+                                ModalNewStep(viewModel : StepDetailsModel(title: "Nova Etapa", stepByStep: "", activityTime: false, frequency: false),completeStep: completeStep)
+                            }
+                            .onTapGesture {
+                                novaEtapa = true
+                            }
+                    }
+                    ForEach(viewModel.stepList) { stepItem in
+                        Button(action: {
+                            selectedStage = stepItem
+                            
+                        }){
+                            Text(stepItem.title)
+                                .foregroundColor(.white)
+                            
+                        }.buttonStyle(StageCard())
+                        .background(CardsGradientStyle())
+                        .cornerRadius(10)
+                        
+                        
+                    }.sheet(item: $selectedStage) { step in
+                        ModalNewStep(viewModel: step, completeStep: completeStep)
+                        
+                    }
                 }
+                .padding(.horizontal)
                 
-            }.navigationTitle(viewModel.treatmentName)
-            .navigationBarTitleDisplayMode(.inline)
-            // MARK: --TODO: adicionar botoes de add e editar quando prontos
-            .navigationBarItems(leading: EmptyView(), trailing: Image(systemName: "clock"))
+                
+                
+            }
+            Spacer()
+            ZStack {
+                RoundedRectangle(cornerRadius: 10.0) .fill(LinearGradient(gradient: Gradient(colors: [Color(.systemBlue), Color(.systemIndigo)]), startPoint: .leading, endPoint: .trailing))
+                    .frame(width: 150, height: 45, alignment: .center)
+                
+                
+                Text("Seguinte").foregroundColor(.white)
+                    .font(.footnote)
+                
+                
+            }.padding()
+            .onTapGesture {
+                /////////////
+            }
             
-        }
+        }.navigationTitle(viewModel.treatmentName)
+        .navigationBarTitleDisplayMode(.inline)
+        // MARK: --TODO: adicionar botoes de add e editar quando prontos
+        .navigationBarItems(leading: EmptyView(), trailing: HStack {
+            Image(systemName: "clock")
+                .onTapGesture {
+                    print("clock")
+                }
+            Image(systemName: "plus")
+                .onTapGesture {
+                    print("plus")
+                }
+        })
+        
         
     }
     func completeStep(id: UUID, title: String, stepByStep: String, activityTime : Bool, frequency : Bool, image : UIImage? = nil) {
@@ -107,6 +114,8 @@ struct TreatmentProStages: View {
 
 struct TreatmentProStages_Previews: PreviewProvider {
     static var previews: some View {
-        TreatmentProStages(viewModel: .init())
+        NavigationView {
+            TreatmentProStages(viewModel: .init())
+        }
     }
 }
